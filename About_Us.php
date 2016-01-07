1edce21394cf38ebf69f797ad5100332e01bca6a
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-01-06 20:55:41 -0500

    Create About_Us.php

---
+++ About_Us.php
@@ -0,0 +1,69 @@
+<?php session_start(); ?>
+
+
+<!doctype html>
+
+<!--This page is designed and built by Yi-Nung Yeh-->
+<head>
+	<meta charset="UTF-8">
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+	<link href="bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="style5.css"> <!-- Resource style -->
+	<link href='https://fonts.googleapis.com/css?family=Racing+Sans+One' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="icon.ico">
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+  	
+	<title>About Us--Smart Meal Planner</title>
+
+</head>
+<body style="overflow-y:scroll;overflow-x:hidden;">
+	
+	
+	<main class="cd-main-content">
+		<div class="cd-fixed-bg cd-bg-1">
+			<h1>About Us</h1>
+		</div> <!-- cd-fixed-bg -->
+
+		<div class="cd-scrolling-bg cd-color-1">
+			<div class="cd-container">
+				<h3>We are a project team of Data and Visual Analytics, Georgia Tech, College of Computing.</h3>
+				<p>We are from Computer Science, Computational Science and Engineering, and Electrical Engineering.</p>
+			</div> <!-- cd-container -->
+		</div> <!-- cd-scrolling-bg -->
+
+		<div class="cd-fixed-bg cd-bg-2">
+			<h1>Acknowledgements</h1>
+		</div> <!-- cd-fixed-bg -->
+
+		<div class="cd-scrolling-bg cd-color-2">
+			<div class="cd-container">
+					<ul>
+					<li>Amazon EC2 &nbsp; <a href="https://aws.amazon.com/ec2/">https://aws.amazon.com/ec2/</a></li>
+					<li>Yummly &nbsp; <a href="http://www.yummly.com/">http://www.yummly.com/</a></li>
+					<li>Allrecipes &nbsp; <a href="http://allrecipes.com/">http://allrecipes.com/</a></li>
+					<li>D3 Data-Driven Documents &nbsp; <a href="http://d3js.org/">http://d3js.org/</a></li>
+					<li>mbostock/d3/Gallery &nbsp; <a href="https://github.com/mbostock/d3/wiki/Gallery">https://github.com/mbostock/d3/wiki/Gallery</a></li>
+					<li>OXXO Studio &nbsp; <a href="http://www.oxxostudio.tw/">http://www.oxxostudio.tw/</a></li>
+					<li>wcc723.github &nbsp; <a href="http://wcc723.github.io/">http://wcc723.github.io/</a></li>
+					<li>Unsplash &nbsp; <a href="https://unsplash.com/">https://unsplash.com/</a></li>
+					<li>Picjumbo &nbsp; <a href="https://picjumbo.com/">https://picjumbo.com/</a></li>
+					<li>Google Font &nbsp; <a href="https://www.google.com/fonts">https://www.google.com/fonts</a></li>
+					<li>Host MyWeb &nbsp; <a href="http://er-webs.com/index.php">http://er-webs.com/index.php</a></li>
+					<li>Walmart Open API &nbsp; <a href="https://developer.walmartlabs.com/">https://developer.walmartlabs.com/</a></li>
+					<li>Numbeo &nbsp; <a href="http://www.numbeo.com/cost-of-living/">http://www.numbeo.com/cost-of-living/</a></li>
+					</ul>
+			</div> <!-- cd-container -->
+		</div> <!-- cd-scrolling-bg -->
+
+		<div class="cd-fixed-bg cd-bg-3">
+			<h1>We Are Smart Meal Planners!</h1>
+		</div> <!-- cd-fixed-bg -->
+	</main> <!-- cd-main-content -->
+	<script type="text/javascript" src="//static.addtoany.com/menu/page.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/main.js"></script> <!-- Resource jQuery -->
+	<script src="bootstrap-modal.js"></script>
+	<script src="app3.js"></script>
+</body>
+</html>

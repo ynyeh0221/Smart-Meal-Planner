e985c98e3f09bdc382de6487b37fc13e5d2aeefe
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-01-06 21:07:17 -0500

    Create Weekly_Shopping_List.php

---
+++ Weekly_Shopping_List.php
@@ -0,0 +1,74 @@
+<?php session_start(); ?>
+
+<!--This page is designed and built by Yi-Nung Yeh-->
+
+<!doctype html>
+<html>
+  <head>
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet'>
+	
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="style4.css" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Amatic+SC' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Coming+Soon' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Patrick+Hand+SC' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Luckiest+Guy' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="icon.ico">
+	<title>Weekly Shopping List--Smart Meal Planner</title>
+  </head>
+  <body>
+  
+  
+  
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  
+
+	
+
+    <script type="text/javascript" src="//static.addtoany.com/menu/page.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="bootstrap-modal.js"></script>
+    <script src="app2.js"></script>
+	<script src="app3.js"></script>
+  </body>
+</html>

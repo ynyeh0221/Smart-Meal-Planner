206d3990ca364af2274707e1baf839825de99530
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-01-06 20:48:19 -0500

    Create How_It_Works.php

---
+++ How_It_Works.php
@@ -0,0 +1,84 @@
+<?php session_start(); ?>
+
+
+<!doctype html>
+<!--This page is designed and built by Yi-Nung Yeh-->
+<!--This description words in this page are provided by Nisheeth Bandaru-->
+<head>
+	<meta charset="UTF-8">
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+	<link href="bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="style.css"> <!-- Resource style -->
+	<link href='https://fonts.googleapis.com/css?family=Palanquin:700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Racing+Sans+One' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="icon.ico">
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+  	
+	<title>How It Works--Smart Meal Planner</title>
+
+</head>
+<body style="overflow-y:scroll;overflow-x:hidden;">
+
+
+	<main class="cd-main-content">
+		<div class="cd-fixed-bg cd-bg-1">
+			<h1>Find Your Perfect Recipes</h1>
+		</div> <!-- cd-fixed-bg -->
+
+		<div class="cd-scrolling-bg cd-color-2">
+			<div class="cd-container">
+			    <h3>Like cooking food but confused what to make?</h3>
+                <h3>Want to explore different recipes?</h3>
+                <h3>Care about nutrition or the amount you spend?</h3>
+				<p>
+                    Let us help delight you! Scroll down to learn more.
+				</p>
+			</div> <!-- cd-container -->
+		</div> <!-- cd-scrolling-bg -->
+
+		<div class="cd-fixed-bg cd-bg-2">
+			<h1>It's for You</h1>
+		</div> <!-- cd-fixed-bg -->
+
+		<div class="cd-scrolling-bg cd-color-3">
+			<div class="cd-container">
+			    <h3>All about you</h3>
+				<p>
+                    Tell us about yourself and your preferences
+				</p>
+				<h3>Save time deciding every day.</h3>
+                <p>
+					Get a weekly meal schedule personalized exclusively for you!
+				</p>
+                <h3>Don't like a suggested recipe? No worries.</h3>
+				<p>
+                    Just click on it to change it.
+				</p>
+			</div> <!-- cd-container -->
+		</div> <!-- cd-scrolling-bg -->
+
+		<div class="cd-fixed-bg cd-bg-3">
+			<h1>Weekly Shopping List</h1>
+		</div> <!-- cd-fixed-bg -->
+
+		<div class="cd-scrolling-bg cd-color-1">
+			<div class="cd-container">
+			    <h3>No more unexpected running out of groceries!</h3>
+				<p>
+                    Get a weekly shopping list with the items you'll need.
+				</p>
+			</div> <!-- cd-container -->
+		</div> <!-- cd-scrolling-bg -->
+
+		<div class="cd-fixed-bg cd-bg-4">
+			<h1>Enjoy Your Meals</h1>
+		</div> <!-- cd-fixed-bg -->
+	</main> <!-- cd-main-content -->
+	<script type="text/javascript" src="//static.addtoany.com/menu/page.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/main.js"></script> <!-- Resource jQuery -->
+	<script src="bootstrap-modal.js"></script>
+	<script src="app3.js"></script>
+</body>
+</html>

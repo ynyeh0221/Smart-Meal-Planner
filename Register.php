0ec7e4bb583a2240b53ddebc2f753f0fda54be99
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-01-06 22:04:54 -0500

    Update Register.php

--- Register.php
+++ Register.php
@@ -12,37 +12,37 @@ $obj = json_decode($data);
 
 if($n==NULL && $p!=NULL)
 {
-  echo "<script> alert('Username is Required.'); </script>";
-	echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
+    echo "<script> alert('Username is Required.'); </script>";
+    echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
 }
 else if($n!=NULL && $p==NULL)
 {
-	echo "<script> alert('Password is Required.'); </script>";
-	echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
+    echo "<script> alert('Password is Required.'); </script>";
+    echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
 }
 else if($n==NULL && $p==NUll)
 {
-	echo "<script> alert('Username and Password are Required.'); </script>";
-	echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
+    echo "<script> alert('Username and Password are Required.'); </script>";
+    echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
 }
 else if($c==NULL)
 {
-  echo "<script> alert('Confirm is Required.'); </script>";
-	echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
+    echo "<script> alert('Confirm is Required.'); </script>";
+    echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
 }
 else if($p!=NULL && $c!=NULL && $p!=$c)
 {
-  echo "<script> alert('Password and Confirm are not the same.'); </script>";
-	echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>";
+    echo "<script> alert('Password and Confirm are not the same.'); </script>";
+    echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>";
 }
 else if($obj[0]!=NULL)
 {
-  echo "<script> alert('Account and Password Already Exist.'); </script>";
-	echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
+    echo "<script> alert('Account and Password Already Exist.'); </script>";
+    echo "<meta http-equiv='Refresh' content='0;URL=./register.html'>"; 
 }
 else
 {
-	$ch = curl_init();
+    $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
